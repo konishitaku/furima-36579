fafcb86bd7511df4b243b0ee2da09ef3c24a7119
konishitaku <88470735+konishitaku@users.noreply.github.com>
2021-09-16 12:44:07 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,10 +70,11 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
-    it 'birthdayが空では登録できない'
+    it 'birthdayが空では登録できない' do
      user.birthday = ''
      user.valid?
      expect(user.errors.full_messages).to include "Birthday can't be blank"
+    end
   end
 end
 end
