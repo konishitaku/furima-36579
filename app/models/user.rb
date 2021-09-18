7afea6362370b0f8f0468291f160f346746212f7
konishitaku <88470735+konishitaku@users.noreply.github.com>
2021-09-18 23:19:56 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'Input full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' } do
   validates :last_name, presence: true
   validates :first_name, presence: true
   end
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   end
-  with_options presence: true, format: { with: /[a-z\d]{6,}/i, message: 'Include both letters and numbers' } do
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' } do
   validates :password, presence: true, length: { minimum: 5 }
   end
   validates :birthday, presence: true
