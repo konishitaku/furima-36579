ff1aa3463c0ea9451e2c54ec5e3fd83edab757d6
konishitaku <88470735+konishitaku@users.noreply.github.com>
2021-09-19 12:23:28 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,15 @@ class User < ApplicationRecord
          
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' } do
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :last_name
+  validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'Input full-width katakana characters' } do
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters' } do
+  validates :last_name_kana
+  validates :first_name_kana
   end
   with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' } do
-  validates :password, presence: true, length: { minimum: 5 }
+  validates :password
   end
   validates :birthday, presence: true
 
