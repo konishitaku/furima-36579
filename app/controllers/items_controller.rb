d0ac31418322ea6b7e0fbad2bdd3c399ea33781f
konishitaku <88470735+konishitaku@users.noreply.github.com>
2021-09-20 18:13:13 +0900

    。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+  @items = Items.new
+  end
+
+  def create
+  Items.create(items_params)
+  end
+
+  private
+  def items_params
+   params.require(:items).permit(:name, :image, :text)
+  end
 end
