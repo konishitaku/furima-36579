2f6b7e251c5f4164f583da49677eee3c8b43cea4
konishitaku <88470735+konishitaku@users.noreply.github.com>
2021-09-30 00:39:04 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :correct_post]
   before_action :correct_post,only: [:edit, :update]
 
   def index
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-   @item = Item.find(params[:id])
    if @item.update(item_params)
     redirect_to item_path
     else
@@ -38,16 +36,12 @@ class ItemsController < ApplicationController
   end
 
   #def destroy
-    #@item = Item.find(params[:id])
     #@item.destroy
     #redirect_to root_path
   #end
 
-  def correct_post
-   @item = Item.find(params[:id])
-   unless @item.user.id == current_user.id
-    redirect_to root_path
-   end
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 
@@ -57,3 +51,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:merchandise, :text, :price, :category_id, :status_id, :area_id, :load_id, :day_id, :image).merge(user_id: current_user.id)
   end
 end
+
+def correct_post
+  unless @item.user.id == current_user.id
+   redirect_to root_path
+  end
+ end
